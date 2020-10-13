8739f159488ba1a9fe82170161b56bdf76656976
T <T@DESKTOP-G170R15>
2020-10-12 22:19:07 -0400

    .

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Tab } from 'semantic-ui-react';
 import ProfilePhotos from './ProfilePhotos';
+import ProfileDescription from './ProfileDescription';
 
 const panes = [
-    { menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane> },
+    { menuItem: 'About', render: () => <ProfileDescription /> },
     { menuItem: 'Photos', render: () => <ProfilePhotos /> },
     { menuItem: 'Activities', render: () => <Tab.Pane>Activities Content</Tab.Pane> },
     { menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane> },
@@ -12,7 +13,7 @@ const panes = [
 
 const ProfileContent = () => {
     return (
-        <Tab menu={{ fluid: true, vertical: true }} menuPosition='right' panes={panes} activeIndex={1} />
+        <Tab menu={{ fluid: true, vertical: true }} menuPosition='right' panes={panes} />
     )
 }
 
--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,35 +1,35 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect } from 'react'
 import { Grid } from 'semantic-ui-react';
 import ProfileHeader from './ProfileHeader';
 import ProfileContent from './ProfileContent';
+import { RouteComponentProps } from 'react-router';
 import { RootStoreContext } from '../../app/stores/rootStore';
-import { RouteComponentProps } from 'react-router-dom';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { observer } from 'mobx-react-lite';
 
 interface RouteParams {
-    username: string;
+    username: string
 }
 
 interface IProps extends RouteComponentProps<RouteParams> {}
 
-const ProfilePage: React.FC<IProps> = ({ match }) => {
+const ProfilePage: React.FC<IProps> = ({match}) => {
     const rootStore = useContext(RootStoreContext);
-    const { loadingProfile, profile, loadProfile } = rootStore.profileStore;
+    const {loadingProfile, profile, loadProfile} = rootStore.profileStore;
 
     useEffect(() => {
         loadProfile(match.params.username);
     }, [loadProfile, match])
 
-    if (loadingProfile) {
-        return (<LoadingComponent content='Loading profile...' />)
-    }
+    if (loadingProfile) return <LoadingComponent content='Loading profile...' />
 
     return (
-        <Grid.Column width={16}>
-            <ProfileHeader profile={profile!} />
-            <ProfileContent />
-        </Grid.Column>
+        <Grid>
+            <Grid.Column width={16}>
+                <ProfileHeader profile={profile!} />
+                <ProfileContent />
+            </Grid.Column>
+        </Grid>
     )
 }
 
